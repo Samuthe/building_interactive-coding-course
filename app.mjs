664681a6531d3bc05ff5ad7473501be8095c4032
Samuthe <ekatisamuel@gmail.com>
2023-03-09 08:51:37 +0300

    app.mjs

---
+++ app.mjs
@@ -0,0 +1,10 @@
+export function myName(name) {
+    return name
+}
+
+export function myNameAgain(nameTwo) {
+    return nameTwo
+}
+
+console.log(myName('world'))
+console.log(myNameAgain('worldAgain'))
